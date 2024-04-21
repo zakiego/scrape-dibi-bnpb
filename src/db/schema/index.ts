609c955e11da57847bacd2378bd5400c31d52aad
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-04-21 13:42:34 +0800

    init

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -2,9 +2,8 @@ import { sql } from "drizzle-orm";
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const data = sqliteTable("data", {
-  id: text("id").primaryKey(),
-  data: text("data", {
-    mode: "json",
-  }).$type<any>(),
-  createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`),
+	id: text("id").primaryKey(),
+	kbi: text("kbi").notNull(),
+	image: text("image"),
+	createdAt: text("created_at").default(sql`(CURRENT_TIMESTAMP)`),
 });
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,47 @@
+import { load } from "cheerio";
 import { z } from "zod";
 import { dbClient, dbSchema } from "~/db";
 import { logger } from "~/utils/log";
 
+const getData = async (id: number) => {
+	const data = await fetch(`https://dibi.bnpb.go.id/xdibi2/read2/${id}`).then(
+		(res) => res.text(),
+	);
+
+	const $ = load(data);
+
+	// find div with class "card-body", then find "row p-4", then find "img
+	const img = $(".card-body .row.p-4 img").attr("src");
+
+	const tanggal = $("span:contains('Tanggal :')").next().text();
+	const kejadian = $("span:contains('Kejadian :')").next().text();
+	const kategori = $("span:contains('Kategori :')").next().text();
+	const lokasi = $("span:contains('Lokasi :')").next().text();
+	const posisi = $("span:contains('Posisi :')").next().text();
+	const sumber = $("span:contains('Sumber :')").next().text();
+	const petugas = $("span:contains('Petugas :')").next().text();
+	const barcode = $("span:contains('Barcode :')").next().attr("src");
+	const keterangan = $("span:contains('Keterangan :')").next().text();
+	const penyebab = $("span:contains('Penyebab :')").next().text();
+
+	return {
+		img,
+		tanggal,
+		kejadian,
+		kategori,
+		lokasi,
+		posisi,
+		sumber,
+		petugas,
+		barcode,
+		keterangan,
+		penyebab,
+	};
+};
+
 const main = async () => {
-  const data = await fetch("https://jsonplaceholder.typicode.com/posts/").then(
-    (res) => res.json(),
-  );
-
-  logger.info("Fetched data");
-
-  const schmea = z.array(
-    z.object({
-      userId: z.number(),
-      id: z.number(),
-      title: z.string(),
-      body: z.string(),
-    }),
-  );
-
-  const parsedData = schmea.parse(data);
-
-  const insert = await dbClient
-    .insert(dbSchema.data)
-    .values(
-      parsedData.map((d) => {
-        return { id: d.id.toString(), data: d };
-      }),
-    )
-    .onConflictDoNothing()
-    .returning();
-
-  logger.info(`Inserted ${insert.length} rows`);
-
-  const result = await dbClient.select().from(dbSchema.data).limit(3);
-
-  console.log(result);
+	const data = await Promise.all([getData(1), getData(65099), getData(65072)]);
+	console.log(data);
 };
 
 main();
